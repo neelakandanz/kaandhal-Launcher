7fb4bf0a0f8c18484e3a0bc7f867f6d8db504039
neelakan24 <neelakan24@gmail.com>
2024-11-09 12:09:02 +0000

    add text and assets folder

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
 import 'app/mvc/view/home/home_screen.dart'; // Add this package for date formatting
+//add assets floder
 
 void main() {
   runApp(const MyApp());
